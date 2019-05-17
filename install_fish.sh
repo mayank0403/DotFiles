8d45a2a6b05cd1faff81bfeedf111bf81501c53b
Mayank <mayankrathee.japan@gmail.com>
2019-05-17 18:53:15 +0530

    Install fish script

---
+++ install_fish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
+chsh -s /usr/local/bin/fish
+
+chsh -s /usr/bin/fish
+
+curl -L https://get.oh-my.fish | fish
+
+omf install ocean
+
+
+
